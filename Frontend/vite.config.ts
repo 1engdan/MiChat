5b6e0739b6d82a6778bf04286c8cfcc7d7122b41
w1tnessbtwwwww <work.w1tnessbtw@mail.ru>
2025-01-28 23:09:03 +0700

    to rework

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '26.197.165.209', // Прослушивание всех сетевых интерфейсов
-    port: 3000, // Порт, на котором будет запущен сервер
+    port: 3002 // Порт, на котором будет запущен сервер
   },
 })
